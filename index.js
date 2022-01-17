c5e4b9fdcd36470080b18263b662cc3ddf60668d
Liam <liamsmitsdorff@gmail.com>
2022-01-17 20:19:36 +0200

    added cloudinary name

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                `https://res.cloudinary.com/${process.env.CLOUDNAME}/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
